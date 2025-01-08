c902756b72e058fda181dd65cb265a46c670eaa5
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-08 16:44:37 +0200

    feat: add projects section placeholder

---
+++ src/Projects.jsx
@@ -0,0 +1,12 @@
+const Projects = () => {
+  return (
+    <div
+      id='projects'
+      className='h-screen w-full max-w-lg flex flex-col justify-center gap-2'
+    >
+      projects
+    </div>
+  );
+};
+
+export default Projects;
