0ad666a49eda5c5ae678e19ed4b1ca58e8304266
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-08 16:45:57 +0200

    style: add padding and change transitions

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,6 +1,8 @@
 import { AnimatePresence, motion } from 'motion/react';
 import { useEffect, useState } from 'react';
 import { CTA } from './CTA';
+import About from './About';
+import Projects from './Projects';
 
 const Content = () => {
   const titles = ['Fullstack Developer', 'UX/UI Designer', 'Music Producer'];
@@ -20,9 +22,8 @@ const Content = () => {
       opacity: 1,
 
       transition: {
-        duration: 0.5,
+        duration: 2,
         ease: 'easeInOut',
-        staggerChildren: 5, // Apply stagger delay to children
       },
     },
   };
@@ -37,9 +38,9 @@ const Content = () => {
       variants={container}
       initial='initial'
       animate='animate'
-      className='w-full h-full flex flex-col items-center justify-center'
+      className='w-full h-full flex flex-col items-center justify-center px-4'
     >
-      <motion.div className='flex flex-col text-left overflow-hidden '>
+      <motion.div className='flex h-screen justify-center flex-col text-left overflow-hidden'>
         <motion.h1
           variants={item}
           transition={{ duration: 0.5, ease: 'easeInOut' }}
@@ -47,22 +48,39 @@ const Content = () => {
         >
           I&apos;m Ethan.
         </motion.h1>
+
+        {/* Titles */}
         <div className='overflow-hidden'>
-          <AnimatePresence mode='wait'>
-            <motion.p
-              key={currentIndex}
-              initial={{ x: '100%' }}
-              variants={item}
-              animate={{ x: 0 }}
-              exit={{ x: '-100%' }}
-              transition={{ duration: 0.4, ease: 'easeInOut' }}
-            >
-              {titles[currentIndex]}
-            </motion.p>
-          </AnimatePresence>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+          >
+            <AnimatePresence mode='wait'>
+              <motion.p
+                key={currentIndex}
+                initial={{ x: '100%' }}
+                variants={item}
+                animate={{ x: 0 }}
+                exit={{ x: '-100%' }}
+                transition={{
+                  duration: 0.5,
+                  ease: 'easeInOut',
+                }}
+              >
+                {titles[currentIndex]}
+              </motion.p>
+            </AnimatePresence>
+          </motion.div>
         </div>
-        <CTA>Learn More</CTA>
+
+        {/* CTA Button */}
+        <motion.div variants={item} className='mt-8'>
+          <CTA scrollToElement='about'>Learn More</CTA>
+        </motion.div>
       </motion.div>
+      <About />
+      <Projects />
     </motion.div>
   );
 };
