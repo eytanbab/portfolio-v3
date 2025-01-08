697047c470f27bf94719afb4db5835386d1b752f
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-08 19:58:29 +0200

    style: fix color change on hover

--- src/ScrollToTop.jsx
+++ src/ScrollToTop.jsx
@@ -3,17 +3,17 @@ import { motion } from 'motion/react';
 const ScrollToTop = () => {
   return (
     <motion.button
+      initial={{ backgroundColor: '#f8fafc' }}
       whileHover={{
         scale: 1.1,
-        color: '#ec4899',
-        transition: { duration: 0.3, ease: 'backInOut' },
+        transition: { duration: 0.3, ease: 'easeInOut' },
+        backgroundColor: '#ec4899',
       }}
       whileTap={{ scale: 0.9 }}
-      className='fixed right-4 bottom-4  bg-slate-50 rounded-full hover:bg-pink-500 p-2'
+      className='fixed right-4 bottom-4 rounded-full p-2'
       onClick={() => scrollTo({ top: 0, behavior: 'smooth' })}
     >
-      <svg
-        className='fill-slate-950'
+      <motion.svg
         height='16px'
         width='16px'
         xmlns='http://www.w3.org/2000/svg'
@@ -25,7 +25,7 @@ const ScrollToTop = () => {
 	l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393
 	C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z'
         />
-      </svg>
+      </motion.svg>
     </motion.button>
   );
 };
