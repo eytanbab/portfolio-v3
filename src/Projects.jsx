c341cd4607ee623804c095e939aac6f6dabd5cbe
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-08 20:21:35 +0200

    feat: add placeholder project

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,11 +1,70 @@
+import { motion, useInView } from 'motion/react';
+import { useRef } from 'react';
+
 const Projects = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const parentVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 1,
+        duration: 0.5,
+        ease: 'easeInOut',
+      },
+    },
+  };
+
+  const childVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+  };
+
   return (
-    <div
+    <motion.div
       id='projects'
-      className='h-screen w-full max-w-lg flex flex-col justify-center gap-2'
+      className='h-screen w-full flex flex-col items-center justify-center'
     >
-      projects
-    </div>
+      {/* Wrapper */}
+      <motion.div
+        variants={parentVariants}
+        initial='hidden'
+        animate={isInView ? 'visible' : 'hidden'}
+        className='w-96 flex flex-col items-start gap-2'
+      >
+        {/* Title */}
+        <motion.div className='relative w-fit' variants={childVariants}>
+          <motion.h1 ref={ref} className='text-2xl'>
+            projects
+          </motion.h1>
+          <motion.div
+            style={{
+              width: isInView ? '100%' : '0%',
+              transition: 'all 0.4s cubic-bezier(0.17, 0.55, 0.55, 1) 1s',
+            }}
+            className='absolute bottom-0.5 w-full h-px rounded-full bg-pink-500'
+          />
+        </motion.div>
+        {/* Projects section */}
+        <motion.div
+          variants={childVariants}
+          className='flex flex-col items-center justify-center rounded-md'
+        >
+          {/* Project */}
+          <motion.div className='relative group'>
+            <motion.h1 className='text-4xl'>Markdown Editor</motion.h1>
+            {/* Underline */}
+            <motion.div className='hidden xl:block absolute w-full left-0 bottom-0  h-1 rounded-full bg-pink-500 transition-transform scale-x-0 group-hover:scale-x-100 duration-300 origin-left ease-in-out' />
+          </motion.div>
+          <div className='mt-1 w-full flex gap-4'>
+            <p>Live site</p>
+            <p>github</p>
+          </div>
+        </motion.div>
+      </motion.div>
+    </motion.div>
   );
 };
 
