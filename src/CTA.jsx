dcb35543d8a513bfcc2c24a9ef1e3bd30a007cdc
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-08 16:45:10 +0200

    feat: add scroll to element

--- src/CTA.jsx
+++ src/CTA.jsx
@@ -2,8 +2,15 @@ import { useRef } from 'react';
 import { useEffect } from 'react';
 
 // eslint-disable-next-line react/prop-types
-export const CTA = ({ children }) => {
+export const CTA = ({ children, scrollToElement }) => {
   const boxRef = useRef(null);
+
+  const handleClickScroll = () => {
+    const element = document.getElementById(scrollToElement);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
   useEffect(() => {
     const boxElement = boxRef.current;
@@ -23,9 +30,12 @@ export const CTA = ({ children }) => {
   }, []);
 
   return (
-    <div className='mt-8 w-full [background:linear-gradient(45deg,theme(colors.cyan.800),theme(colors.slate.950)_50%,theme(colors.slate.800))_padding-box,conic-gradient(from_var(--border-angle),theme(colors.slate.600/.48)_80%,_theme(colors.pink.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.slate.600/.48))_border-box] rounded-full border border-transparent animate-border p-0.5'>
-      <button className='w-full rounded-full py-1 bg-slate-950'>
-        Learn More
+    <div className='w-full [background:linear-gradient(45deg,theme(colors.cyan.800),theme(colors.slate.950)_50%,theme(colors.slate.800))_padding-box,conic-gradient(from_var(--border-angle),theme(colors.slate.600/.48)_80%,_theme(colors.pink.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.slate.600/.48))_border-box] rounded-full border border-transparent animate-border p-0.5'>
+      <button
+        onClick={handleClickScroll}
+        className='w-full rounded-full py-1 bg-slate-950'
+      >
+        {children}
       </button>
     </div>
   );
