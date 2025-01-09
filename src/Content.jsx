af4df7775f5e34c83b659bcea339f66a66d4391b
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-09 20:00:23 +0200

    chore: removed unnecessary motion variant

--- src/Content.jsx
+++ src/Content.jsx
@@ -5,25 +5,8 @@ import Skills from './Skills';
 import Intro from './Intro';
 
 const Content = () => {
-  const container = {
-    initial: { opacity: 0 },
-    animate: {
-      opacity: 1,
-
-      transition: {
-        duration: 2,
-        ease: 'easeInOut',
-      },
-    },
-  };
-
   return (
-    <motion.div
-      variants={container}
-      initial='initial'
-      animate='animate'
-      className='w-full flex flex-col items-center justify-center px-4'
-    >
+    <motion.div className='w-full flex flex-col items-center justify-center px-4'>
       <Intro />
       <About />
       <Skills />
