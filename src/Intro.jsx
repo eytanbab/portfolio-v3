da71786b267e9d2572a2517e8f171721559e9d19
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-09 20:38:34 +0200

    feat: add contact links

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import { AnimatePresence, motion } from 'motion/react';
 import CTA from './CTA';
+import { FaGithub, FaLinkedin } from 'react-icons/fa6';
+import { PiReadCvLogo } from 'react-icons/pi';
+import { MdAlternateEmail } from 'react-icons/md';
 
 const Intro = () => {
   const titles = ['Fullstack Developer', 'UX/UI Designer', 'Music Producer'];
@@ -45,7 +48,7 @@ const Intro = () => {
         variants={parentVariants}
         initial='hidden'
         animate='visible'
-        className='max-w-96 flex flex-col items-center gap-1 relative'
+        className='max-w-96 flex flex-col items-center gap-1  relative'
       >
         <motion.h1 variants={childVariants} className='text-4xl font-semibold '>
           I&apos;m Ethan.
@@ -81,7 +84,7 @@ const Intro = () => {
               ease: 'ease-in-out',
               repeat: Infinity,
             }}
-            className='mt-4 px-2 py-1 rounded-md flex items-center justify-center gap-2 text-emerald-500 relative'
+            className='px-2 py-1 rounded-md flex items-center justify-center gap-2 text-emerald-500 relative'
           >
             {/* Work status text */}
             <span className='before:content-[""] before:absolute before:inset-0 before:w-full before:bg-emerald-500/50 before:blur-2xl before:rounded-full'>
@@ -91,8 +94,53 @@ const Intro = () => {
             <div className='size-2 bg-emerald-500 rounded-full' />
           </motion.div>
         </motion.div>
+        {/* Links */}
+        <motion.div
+          className='mt-4 flex items-center justify-center gap-4'
+          variants={childVariants}
+        >
+          <motion.a
+            whileTap={{ scale: 0.9 }}
+            href='https://github.com/eytanbab'
+            target='_blank'
+          >
+            <FaGithub
+              size={24}
+              className='fill-slate-fill-300 hover:fill-pink-500 transition-colors duration-300 ease-in-out'
+            />
+          </motion.a>
+          <motion.a
+            whileTap={{ scale: 0.9 }}
+            href='https://www.linkedin.com/in/ethan-babaev/'
+            target='_blank'
+          >
+            <FaLinkedin
+              size={24}
+              className='fill-slate-300 hover:fill-pink-500 transition-colors duration-300 ease-in-out'
+            />
+          </motion.a>
+          <motion.a
+            whileTap={{ scale: 0.9 }}
+            href='https://drive.google.com/file/d/1yCg-B6YAWPD5iIJHXEJX32xnSSScFd9D/view?usp=sharing'
+            target='_blank'
+          >
+            <PiReadCvLogo
+              size={24}
+              className='fill-slate-300 hover:fill-pink-500 transition-colors duration-300 ease-in-out'
+            />
+          </motion.a>
+          <motion.a
+            whileTap={{ scale: 0.9 }}
+            href='mailto:ethan.babaev@gmail.com'
+          >
+            <MdAlternateEmail
+              size={24}
+              className='fill-slate-300 hover:fill-pink-500 transition-colors duration-300 ease-in-out'
+            />
+          </motion.a>
+        </motion.div>
         {/* CTA Button */}
-        <motion.div variants={childVariants} className='mt-8 w-full'>
+        <motion.div variants={childVariants} className='mt-4 w-full'>
           <CTA scrollToElement='about'>Learn More</CTA>
         </motion.div>
       </motion.div>
