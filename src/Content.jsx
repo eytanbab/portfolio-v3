f78a3530e076cbfc2c1bcf7fdf24323b1b8a7198
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-10 19:13:29 +0200

    feat: add custom scrollbar

--- src/Content.jsx
+++ src/Content.jsx
@@ -4,10 +4,12 @@ import Projects from './Projects/Projects';
 import Skills from './Skills';
 import Intro from './Intro';
 import Music from './Music';
+import Scrollbar from './Scrollbar';
 
 const Content = () => {
   return (
     <motion.div className='w-full flex flex-col items-center justify-center px-4'>
+      <Scrollbar />
       <Intro />
       <About />
       <Skills />
---
+++ src/Scrollbar.jsx
@@ -0,0 +1,60 @@
+import {
+  AnimatePresence,
+  motion,
+  useMotionValueEvent,
+  useScroll,
+  useSpring,
+} from 'framer-motion';
+import { useState } from 'react';
+import { FaArrowDown } from 'react-icons/fa6';
+
+const Scrollbar = () => {
+  const { scrollYProgress } = useScroll();
+  const [text, setText] = useState('Scroll');
+
+  useMotionValueEvent(scrollYProgress, 'change', (latest) => {
+    latest === 0 ? setText('Scroll') : setText('');
+  });
+
+  const scaleY = useSpring(scrollYProgress, {
+    stiffness: 300,
+    damping: 50,
+    restDelta: 0.001,
+  });
+
+  return (
+    <div className='hidden xl:block'>
+      <motion.div
+        style={{ borderRadius: '9999px' }}
+        className={`fixed bg-slate-900 w-2 h-96 top-1/2 -translate-y-1/2 right-4`}
+      >
+        <AnimatePresence>
+          {text && (
+            <motion.div
+              animate={{ opacity: [0.25, 1, 1, 0.25] }}
+              transition={{
+                duration: 2.5,
+                ease: 'easeInOut',
+                repeat: Infinity,
+              }}
+              className='fixed top-1/2 -translate-1/2 right-4 text-slate-300 flex flex-col items-center animate-pulse'
+            >
+              <motion.span>{text}</motion.span>
+              <FaArrowDown />
+            </motion.div>
+          )}
+        </AnimatePresence>
+        <motion.div
+          style={{
+            scaleY: scaleY,
+            borderRadius: '9999px',
+            originY: 0,
+          }}
+          className={`absolute h-96 bg-pink-500 w-2 top-0 -translate-y-1/2 right-0`}
+        ></motion.div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Scrollbar;
