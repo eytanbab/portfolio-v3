bb098f6f21d5ced134cb07efb2a39c8707e48fe4
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-11 17:45:43 +0200

    style: change about animation

--- src/About.jsx
+++ src/About.jsx
@@ -27,6 +27,18 @@ const About = () => {
     },
   };
 
+  const aboutParentVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2,
+        duration: 0.5,
+        ease: 'easeInOut',
+      },
+    },
+  };
+
   return (
     <motion.div
       id='about'
@@ -53,135 +65,143 @@ const About = () => {
           />
         </motion.div>
         {/* About me section */}
-        <motion.div variants={childVariants}>
-          <motion.p className='font-light text-slate-300'>
-            Hi, I’m <span className='font-medium text-slate-50'>Ethan -</span> a{' '}
-            <span className='font-medium text-slate-50'>
-              Fullstack developer
-            </span>{' '}
-            with a{' '}
-            <span className='font-medium text-slate-50'>
-              BSc in Software Engineering.
-            </span>{' '}
-          </motion.p>
-          <motion.p className='font-light text-slate-300'>
-            I’ve been building{' '}
-            <span className='font-medium text-slate-50'>
-              user-friendly web applications
-            </span>{' '}
-            with tools like{' '}
-            <span className='font-medium text-slate-50'>
-              React, Next.js, and TypeScript,
-            </span>{' '}
-            creating projects like a{' '}
-            <span className='font-medium text-slate-50'>
-              financial tracker, markdown editor
-            </span>{' '}
-            and a{' '}
-            <span className='font-medium text-slate-50'>
-              job application tracker
-            </span>{' '}
-            from scratch.
-          </motion.p>
-          <motion.p className='font-light text-slate-300'>
-            I’m also exploring{' '}
-            <span className='font-medium text-slate-50'>UX/UI design,</span>{' '}
-            with recent work including a{' '}
-            <span className='font-medium text-slate-50'>
-              Spotify remake in Figma.
-            </span>{' '}
-          </motion.p>
-          <motion.p className='font-light text-slate-300'>
-            In addition to my technical skills, I’ve been{' '}
-            <span className='font-medium text-slate-50'>producing music</span>{' '}
-            for the past{' '}
-            <span className='font-medium text-slate-50'>12 years.</span>{' '}
-          </motion.p>
-          <motion.p className='font-light text-slate-300'>
-            I’m currently{' '}
-            <span className='font-medium text-slate-50'>looking for a job</span>{' '}
-            where I can apply my skills and passion to create{' '}
-            <span className='font-medium text-slate-50'>
-              impactful digital experiences.
-            </span>
-          </motion.p>
-          {/* Divider */}
-          <motion.div className='mt-1 flex gap-1 w-full items-center'>
-            <div className='bg-slate-600/75 flex-1 h-px' />
-            <div className='bg-slate-600 size-1 rounded-full' />
-            <div className='bg-slate-600/75 flex-1 h-px' />
-          </motion.div>
-        </motion.div>
-
-        {/* Work experience */}
-        <motion.div
-          variants={childVariants}
-          className='flex flex-col items-start gap-2 w-full'
-        >
-          {/* Job title */}
-          <motion.div className='text-slate-50 w-full'>
-            <motion.h1 className='font-medium'>
-              LLM Code Refactoring Engineer
-            </motion.h1>
-            <motion.div className='flex justify-between w-full'>
-              <p className='text-sm'>MLG International</p>
-              <p className='text-xs font-light '>Apr. 2024 - current</p>
+        <motion.div variants={aboutParentVariants}>
+          <motion.div variants={childVariants}>
+            <motion.p className='font-light text-slate-300'>
+              Hi, I’m <span className='font-medium text-slate-50'>Ethan -</span>{' '}
+              a{' '}
+              <span className='font-medium text-slate-50'>
+                Fullstack developer
+              </span>{' '}
+              with a{' '}
+              <span className='font-medium text-slate-50'>
+                BSc in Software Engineering.
+              </span>{' '}
+            </motion.p>
+            <motion.p className='font-light text-slate-300'>
+              I’ve been building{' '}
+              <span className='font-medium text-slate-50'>
+                user-friendly web applications
+              </span>{' '}
+              with tools like{' '}
+              <span className='font-medium text-slate-50'>
+                React, Next.js, and TypeScript,
+              </span>{' '}
+              creating projects like a{' '}
+              <span className='font-medium text-slate-50'>
+                financial tracker, markdown editor
+              </span>{' '}
+              and a{' '}
+              <span className='font-medium text-slate-50'>
+                job application tracker
+              </span>{' '}
+              from scratch.
+            </motion.p>
+            <motion.p className='font-light text-slate-300'>
+              I’m also exploring{' '}
+              <span className='font-medium text-slate-50'>UX/UI design,</span>{' '}
+              with recent work including a{' '}
+              <span className='font-medium text-slate-50'>
+                Spotify remake in Figma.
+              </span>{' '}
+            </motion.p>
+            <motion.p className='font-light text-slate-300'>
+              In addition to my technical skills, I’ve been{' '}
+              <span className='font-medium text-slate-50'>producing music</span>{' '}
+              for the past{' '}
+              <span className='font-medium text-slate-50'>12 years.</span>{' '}
+            </motion.p>
+            <motion.p className='font-light text-slate-300'>
+              I’m currently{' '}
+              <span className='font-medium text-slate-50'>
+                looking for a job
+              </span>{' '}
+              where I can apply my skills and passion to create{' '}
+              <span className='font-medium text-slate-50'>
+                impactful digital experiences.
+              </span>
+            </motion.p>
+            {/* Divider */}
+            <motion.div className='mt-1 flex gap-1 w-full items-center'>
+              <div className='bg-slate-600/75 flex-1 h-px' />
+              <div className='bg-slate-600 size-1 rounded-full' />
+              <div className='bg-slate-600/75 flex-1 h-px' />
             </motion.div>
           </motion.div>
-          {/* Job description */}
-          <motion.ul className='text-sm space-y-1 text-slate-200'>
-            <li className='ml-4 list-disc font-light'>
-              <span className='text-slate-50 font-medium'>Review</span> and{' '}
-              <span className='text-slate-50 font-medium'>
-                correct code generated by LLMs
-              </span>{' '}
-              across multiple <span></span>
-              <span className='text-slate-50 font-medium'>
-                programming languages and frameworks,
-              </span>{' '}
-              including{' '}
-              <span className='text-slate-50 font-medium'>
-                Python, JavaScript, React,
-              </span>{' '}
-              and more.
-            </li>
-            <li className='ml-4 list-disc'>
-              <span className='text-slate-50 font-medium'>Ensure</span> the
-              <span className='text-slate-50 font-medium'>
-                {' '}
-                accuracy, efficiency, and functionality
-              </span>{' '}
-              of{' '}
-              <span className='text-slate-50 font-medium'>
-                LLM generated code
-              </span>{' '}
-              by <span className='text-slate-50 font-medium'>debugging</span>{' '}
-              and <span className='text-slate-50 font-medium'>optimizing</span>{' '}
-              it.
-            </li>
-          </motion.ul>
-          {/* Divider */}
-          <motion.div className='flex gap-1 w-full items-center'>
-            <div className='bg-slate-600/75 flex-1 h-px' />
-            <div className='bg-slate-600 size-1 rounded-full' />
-            <div className='bg-slate-600/75 flex-1 h-px' />
-          </motion.div>
-        </motion.div>
 
-        {/* College and courses */}
-        {/* SCE */}
-        <motion.div variants={childVariants} className='w-full'>
-          <motion.div className='text-slate-50 w-full'>
-            <motion.h1 className='font-medium'>
-              BSc in Software Engineering
-            </motion.h1>
-            <motion.div className='flex justify-between w-full'>
-              <p className='text-xs'>
-                SCE - Sami Shamoon College of Engineering
+          {/* Work experience */}
+          <motion.div
+            variants={childVariants}
+            className='flex flex-col items-start gap-2 w-full'
+          >
+            {/* Job title */}
+            <motion.div className='text-slate-50 w-full'>
+              <motion.h1 className='font-medium'>
+                LLM Code Refactoring Engineer
+              </motion.h1>
+              <motion.div className='flex justify-between w-full'>
+                <p className='text-sm'>MLG International</p>
+                <p className='text-xs font-light '>Apr. 2024 - current</p>
+              </motion.div>
+            </motion.div>
+            {/* Job description */}
+            <motion.ul className='text-sm space-y-1 text-slate-200'>
+              <li className='ml-4 list-disc font-light'>
+                <span className='text-slate-50 font-medium'>Review</span> and{' '}
+                <span className='text-slate-50 font-medium'>
+                  correct code generated by LLMs
+                </span>{' '}
+                across multiple <span></span>
+                <span className='text-slate-50 font-medium'>
+                  programming languages and frameworks,
+                </span>{' '}
+                including{' '}
+                <span className='text-slate-50 font-medium'>
+                  Python, JavaScript, React,
+                </span>{' '}
+                and more.
+              </li>
+              <li className='ml-4 list-disc'>
+                <span className='text-slate-50 font-medium'>Ensure</span> the
+                <span className='text-slate-50 font-medium'>
+                  {' '}
+                  accuracy, efficiency, and functionality
+                </span>{' '}
+                of{' '}
+                <span className='text-slate-50 font-medium'>
+                  LLM generated code
+                </span>{' '}
+                by <span className='text-slate-50 font-medium'>debugging</span>{' '}
+                and{' '}
+                <span className='text-slate-50 font-medium'>optimizing</span>{' '}
+                it.
+              </li>
+            </motion.ul>
+            {/* Divider */}
+            <motion.div className='flex gap-1 w-full items-center'>
+              <div className='bg-slate-600/75 flex-1 h-px' />
+              <div className='bg-slate-600 size-1 rounded-full' />
+              <div className='bg-slate-600/75 flex-1 h-px' />
+            </motion.div>
+          </motion.div>
+
+          {/* College and courses */}
+          {/* SCE */}
+          <motion.div variants={childVariants} className='w-full'>
+            <motion.div className='text-slate-50 w-full'>
+              <motion.h1 className='font-medium'>
+                BSc in Software Engineering
+              </motion.h1>
+              <motion.div className='flex justify-between w-full'>
+                <p className='text-xs'>
+                  SCE - Sami Shamoon College of Engineering
+                </p>
+                <p className='text-xs font-light'>Oct 2019 – Sep 2023</p>
+              </motion.div>
+              <p className='text-sm font-light mt-1'>
+                Be&apos;er Sheva, Israel
               </p>
-              <p className='text-xs font-light'>Oct 2019 – Sep 2023</p>
             </motion.div>
-            <p className='text-sm font-light mt-1'>Be&apos;er Sheva, Israel</p>
           </motion.div>
         </motion.div>
 
